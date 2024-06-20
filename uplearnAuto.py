00766febfcadb75b742db8a71238cfc4fabc253f
duckFuss <dtfussey@icloud.com>
2024-06-20 20:13:40 +0100

    optimisation to speed

--- uplearnAuto.py
+++ uplearnAuto.py
@@ -1,34 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 class uplearnLazy():
     def __init__(self) -> None:
         self.browser = webdriver.Firefox()
-        self.t = 1
     def login(self):
-        usrnme = self.browser.find_element(By.ID, "input-1")
-        pwd = self.browser.find_element(By.ID, "input-2")
-        enterButton = self.browser.find_element(By.TAG_NAME, "button")
-        usrnme.send_keys("fussd001@chartersschool.org.uk")
-        pwd.send_keys("uplearnG0ldf!sh")
-        enterButton.click()
+        usrnme = None
+        while not usrnme:
+            try:
+                usrnme = self.browser.find_element(By.ID, "input-1")
+                pwd = self.browser.find_element(By.ID, "input-2")
+                enterButton = self.browser.find_element(By.TAG_NAME, "button")
+                usrnme.send_keys("fussd001@chartersschool.org.uk")
+                pwd.send_keys("uplearnG0ldf!sh")
+                enterButton.click()
+            except NoSuchElementException:
+                time.sleep(1)
     def answerQuestion(self, element):
-        #time.sleep(0.5)
         Button = self.browser.find_element(By.XPATH, element)
         self.browser.execute_script("arguments[0].click();", Button)
         submitButton = self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div/main/div/div[2]/div[2]/div/button")
         self.browser.execute_script("arguments[0].click();", submitButton)
-        time.sleep(0.25)
-        continueButton = self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div/main/div/div[2]/div[2]/div/button[2]")
+        time.sleep(1.1)
+        continueButton = None
+        while not continueButton:
+            try:
+                continueButton = self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div/main/div/div[2]/div[2]/div/button[2]")
+            except NoSuchElementException:
+                time.sleep(0.1)
+                print("waited")
         self.browser.execute_script("arguments[0].click();", continueButton)
     def returnToQuiz(self, quizElement):
-        time.sleep(self.t)
-        return1 = self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div/header/div/span[3]/a")
+        return1, return2 = None, None
+        while not return1:
+            try:
+                return1 = self.browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div/header/div/span[3]/a")
+            except NoSuchElementException:
+                time.sleep(0.2)
         self.browser.execute_script("arguments[0].click();", return1)
-        time.sleep(self.t)
-        return2 = self.browser.find_element(By.XPATH, quizElement)
+        while not return2:
+            try:
+                return2 = self.browser.find_element(By.XPATH, quizElement)
+            except NoSuchElementException:
+                time.sleep(0.2)
         self.browser.execute_script("arguments[0].click();", return2)
 
 class physicsBot(uplearnLazy):
@@ -39,9 +55,14 @@ class physicsBot(uplearnLazy):
         self.answers = {"B":"/html/body/div[1]/div/div/div/main/div/div[2]/div[1]/div[1]/div/section/div/label[2]",
                         "C":"/html/body/div[1]/div/div/div/main/div/div[2]/div[1]/div[1]/div/section/div/label[3]"}
     def quizKinematics(self):
-        time.sleep(self.t)
-        startButton = self.browser.find_element(By.XPATH, '//button[@class="sc-EElJA sc-eXuhCa jgIuES dpmZGi"]')
-        startButton.click()
+        time.sleep(0.5)
+        startButton = None
+        while not startButton:
+            try:
+                startButton = self.browser.find_element(By.XPATH, '//button[@class="sc-EElJA sc-eXuhCa jgIuES dpmZGi"]')
+                startButton.click()
+            except NoSuchElementException: 
+                time.sleep(0.5)
         for i in range(3):
             self.answerQuestion(self.answers["B"])
             self.answerQuestion(self.answers["C"])
@@ -60,18 +81,18 @@ class chemBot(uplearnLazy):
                         4: "/html/body/div[1]/div/div/div/main/div/div[2]/div[1]/div[1]/div/section/div/label[5]/p/span/span",
                         5: "/html/body/div[1]/div/div/div/main/div/div[2]/div[1]/div[1]/div/section/div/label[4]/p/span/span"}
     def quizForcesOnElectrons(self):
-        time.sleep(self.t)
-        startButton = self.browser.find_element(By.XPATH, '//button[@class="sc-EElJA sc-eXuhCa jgIuES dpmZGi"]')
-        startButton.click()
+        time.sleep(0.5)
+        startButton = None
+        while not startButton:
+            try:
+                startButton = self.browser.find_element(By.XPATH, '//button[@class="sc-EElJA sc-eXuhCa jgIuES dpmZGi"]')
+                startButton.click()
+            except NoSuchElementException: 
+                time.sleep(0.5)
         for i in range(6):
             self.answerQuestion(self.forcesOnElectronsAnswers[i])
         self.returnToQuiz("/html/body/div[1]/div/div/ul/li[4]/a/div/div[1]")
 
-#physDuck = physicsBot()
-#physDuck.quizKinematics()
-
-chemDuck = chemBot()
+physDuck = physicsBot()
 while True:
-    chemDuck.quizForcesOnElectrons()
-
-
+    physDuck.quizKinematics()
